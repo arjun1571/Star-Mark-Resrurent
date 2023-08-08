28da4450bb9fe99b3a113fc67914a0b22c4984e0
Arjun <arjundasback515@gmail.com>
2023-08-08 12:47:36 +0600

    navigation create and dynamic value on navigation item set done

--- src/LayOut/Main.jsx
+++ src/LayOut/Main.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Component/Footer/Footer";
+import NavBar from "../Component/NavBar/NavBar";
 
 
 const Main = () => {
     return (
         <div>
+            <NavBar></NavBar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
