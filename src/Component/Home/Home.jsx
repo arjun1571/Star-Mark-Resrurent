3b9ac9626c3c22609fd600de768d325d3c464510
Arjun <arjundasback515@gmail.com>
2023-08-08 13:29:30 +0600

    banner carosel create using react awesome component and fixed the navigation position and cheange the background opecity

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,9 +1,10 @@
+import Banner from "./Banner/Banner";
 
 
 const Home = () => {
     return (
         <div>
-            thisj is home
+            <Banner></Banner>
         </div>
     );
 };
--- src/Component/NavBar/NavBar.jsx
+++ src/Component/NavBar/NavBar.jsx
@@ -11,7 +11,7 @@ const NavBar = () => {
   );
   return (
     <>
-      <div className="navbar bg-base-100">
+      <div className="navbar fixed z-10  text-white max-w-screen-xl bg-black bg-opacity-30">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
