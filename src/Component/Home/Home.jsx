7389297cfe48ac7fa18c485428e51299db0cfc9d
Arjun <arjundasback515@gmail.com>
2023-08-09 13:12:39 +0600

    review section creation on react awasome slider use and data pass to dynamic

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -2,6 +2,7 @@ import BoosBanner from "../BoosBanner/BoosBanner";
 import OurMenu from "../OurMenu/OurMenu";
 import PapularItem from "../PapularItem/PapularItem";
 import Recomendas from "../Recomendas/Recomendas";
+import TestiMonial from "../Testimonial/TestiMonial";
 import Banner from "./Banner/Banner";
 import Categories from "./Categories/Categories";
 
@@ -15,6 +16,7 @@ const Home = () => {
             <PapularItem></PapularItem>
             <Recomendas></Recomendas>
             <OurMenu></OurMenu>
+            <TestiMonial></TestiMonial>
         </div>
     );
 };
---
+++ src/Component/Testimonial/TestiMonial.jsx
@@ -0,0 +1,45 @@
+import SectionTitle from "../SectionTitle/SectionTitle";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+import { useEffect, useState } from "react";
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
+import img from "../../assets/home/quote-left 1.png"
+
+const TestiMonial = () => {
+  const [datas, setDatas] = useState([]);
+  useEffect(() => {
+    fetch("reviews.json")
+      .then((res) => res.json())
+      .then((data) => setDatas(data));
+  }, []);
+
+  return (
+    <div className="my-20">
+      <SectionTitle
+        title={"What Our Clients Say"}
+        subtitle={"TESTIMONIALS"}
+      ></SectionTitle>
+      <div className="my-10">
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+          {datas.map((data) => (
+            <SwiperSlide key={data._id}>
+              <div className="lg:mx-20 md:mx-12 mx-10 text-center items-center">
+                <Rating style={{ maxWidth: 180 }} className="mx-auto" value={data.rating} readOnly />
+                <img className="mx-auto my-5" src={img} alt="" />
+                <p className="my-3">{data.details}</p>
+                <h1 className="text-4xl font-bold text-yellow-500">
+                  {data.name}
+                </h1>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
+};
+
+export default TestiMonial;
